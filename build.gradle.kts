e3539ebc8477586a673f9e2be47a4606856c4fe0
JellyBrick <shlee1503@naver.com>
2021-10-31 21:24:34 +0900

    Gradle (#2)

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    id("org.jmailen.kotlinter") version "3.6.0"
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+val githubRepo = project.property("github.repo") as String
+
+tasks.withType<KotlinCompile> {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation(group = "org.jetbrains.kotlin", name = "kotlin-stdlib-jdk8")
+
+    // Use the Kotlin test library.
+    testImplementation(group = "org.jetbrains.kotlin", name = "kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation(group = "org.jetbrains.kotlin", name = "kotlin-test-junit")
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    from(sourceSets["main"].allSource)
+    archiveClassifier.set("sources")
+}
